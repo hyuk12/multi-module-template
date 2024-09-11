546f0e30f9b3668f64b942ce6646359fc95dcfca
hyuk12 <hotman7025@naver.com>
2024-09-11 14:17:15 +0900

    feat: 기본적인 동작을 위한 구성

--- src/main/java/com/multi/template/TemplateApplication.java
+++ src/main/java/com/multi/template/TemplateApplication.java
@@ -2,8 +2,12 @@ package com.multi.template;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.template.api", "com.template.middleware", "com.template.datastorage"},
+	exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class}
+)
 public class TemplateApplication {
 
 	public static void main(String[] args) {
